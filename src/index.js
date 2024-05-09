1b285a48d533092a7a24b9d8906b0d5282331d54
pixie-cheeks <121865202+pixie-cheeks@users.noreply.github.com>
2024-05-09 15:44:45 +0530

    Meet requirements on dropdown module

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 /* eslint-disable import/no-import-module-exports */
 
 import './style.css';
-import createDropdown from './scripts/dropdown';
+import createDropdownManager from './scripts/dropdown';
 
-createDropdown(
-  document.querySelector('.button'),
-  document.querySelector('.dropdown__menu'),
-  'dropdown__menu--hidden',
+const manager = createDropdownManager('dropdown__menu--hidden');
+
+manager.addDropdown(
+  document.querySelector('.js-one .button'),
+  document.querySelector('.js-one .dropdown__menu'),
+  'click',
+);
+
+manager.addDropdown(
+  document.querySelector('.js-two .button'),
+  document.querySelector('.js-two .dropdown__menu'),
+  'click',
 );
 
 if (process.env.NODE_ENV === 'development' && module.hot) {
--- src/scripts/dropdown.js
+++ src/scripts/dropdown.js
@@ -1,35 +1,101 @@
-const bindHoverEvents = (button, menu, visibilityClass) => {
-  button.addEventListener('mouseenter', () => {
-    menu.classList.remove(visibilityClass);
-  });
-
-  button.addEventListener('mouseleave', (e) => {
-    if (e.toElement.closest(`.${menu.className}`)) return;
-    menu.classList.add(visibilityClass);
-  });
-
-  menu.addEventListener('mouseleave', () => {
-    menu.classList.add(visibilityClass);
-  });
+const eventBinder = {
+  eventCache: [],
+  bindEvent(trigger, event, listener) {
+    trigger.addEventListener(event, listener);
+    this.eventCache.push({ trigger, event, listener });
+  },
+  unbindEvent(trigger, event) {
+    this.eventCache
+      .filter((elem) => elem.trigger === trigger && elem.event === event)
+      .forEach((elem) => {
+        elem.trigger.removeEventListener(elem.event, elem.listener);
+      });
+  },
 };
 
-const createDropdown = (
-  button,
-  menu,
-  visibilityClass,
-  triggerEvent = 'click',
-) => {
-  if (!/^(click|hover)$/g.test(triggerEvent)) {
-    throw new Error('Only accepted event parameters are hover or click.');
-  }
+function createDropdownManager(invisibilityClass) {
+  let visibleDropdown = null;
 
-  if (triggerEvent === 'hover') {
-    bindHoverEvents(button, menu, visibilityClass);
-    return;
-  }
-  button.addEventListener(triggerEvent, () => {
-    menu.classList.toggle(visibilityClass);
-  });
-};
+  const hideDropdown = (menu) => {
+    menu.classList.add(invisibilityClass);
+  };
 
-export default createDropdown;
+  const showDropdown = (menu) => {
+    menu.classList.remove(invisibilityClass);
+  };
+
+  const bindHoverEvents = (button, menu) => {
+    eventBinder.bindEvent(button, 'mouseenter', () => {
+      showDropdown(menu, invisibilityClass);
+    });
+    eventBinder.bindEvent(button, 'mouseleave', (e) => {
+      if (e.toElement.closest(`.${menu.className}`)) return;
+
+      hideDropdown(menu, invisibilityClass);
+    });
+
+    eventBinder.bindEvent(menu, 'mouseleave', () => {
+      hideDropdown(menu, invisibilityClass);
+    });
+  };
+
+  const bindClickEvents = (button, menu) => {
+    eventBinder.bindEvent(button, 'click', () => {
+      if (visibleDropdown === null) {
+        showDropdown(menu);
+        visibleDropdown = menu;
+        return;
+      }
+
+      if (visibleDropdown === menu) {
+        hideDropdown(menu);
+        visibleDropdown = null;
+      } else {
+        hideDropdown(visibleDropdown);
+        showDropdown(menu);
+        visibleDropdown = menu;
+      }
+    });
+  };
+
+  const unbindHoverEvents = (trigger, menu) => {
+    eventBinder.unbindEvent(trigger, 'mouseenter');
+    eventBinder.unbindEvent(trigger, 'mouseleave');
+    eventBinder.unbindEvent(menu, 'mouseleave');
+  };
+  const unbindClickEvents = (trigger) => {
+    eventBinder.unbindEvent(trigger, 'click');
+  };
+
+  const addDropdown = (trigger, menu, triggerEvent = 'click') => {
+    if (!/^(click|hover)$/g.test(triggerEvent)) {
+      throw new Error(
+        'Only accepted triggerEvent parameters are "hover" or "click".',
+      );
+    }
+
+    if (triggerEvent === 'click') {
+      bindClickEvents(trigger, menu);
+    } else {
+      bindHoverEvents(trigger, menu);
+    }
+  };
+
+  const removeDropdown = (trigger, menu, triggerEvent) => {
+    if (!/^(click|hover)$/g.test(triggerEvent)) {
+      throw new Error(
+        'Only accepted triggerEvent parameters are "hover" or "click".',
+      );
+    }
+
+    if (triggerEvent === 'click') {
+      unbindClickEvents(trigger);
+    } else {
+      unbindHoverEvents(trigger, menu);
+    }
+  };
+
+  return { addDropdown, removeDropdown };
+}
+
+export default createDropdownManager;
